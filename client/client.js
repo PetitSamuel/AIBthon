592019d2907644e00531ecd58fe753c313abe768
DaVinciTachyon <davincitachyon@gmail.com>
2018-11-10 13:00:09 +0000

    working on buy deal

--- client/client.js
+++ client/client.js
@@ -8,34 +8,48 @@ $("#dialog").dialog({
     show : "blind",
     hide : "blind"
   });
+$("#buyDialog").dialog({
+    autoOpen : false,
+    title: 'Buy',
+    draggable: false,
+    modal : false,
+    height: 700,
+    width: 1100,
+    show : "blind",
+    hide : "blind"
+  });
 $(document).ready(function(){
+  $.get(
+    "http://127.0.0.1:5000/deals",
+    function(response) {
+      if(response.items === 0){
+        alert('No deals to display');
+        return;
+      }
+      // append to table
+      var rowsAsHtml = '';
+      //make pretty
+      $.each(response.data, function (key, value) {
+        rowsAsHtml += '<tr id="' + value.id + '">';
+        rowsAsHtml += '<td>' + value.item + '</td>';
+        rowsAsHtml += '<td>' + value.price + '</td>';
+        rowsAsHtml += '<td>' + value.location + '</td>';
+        rowsAsHtml += '<td>' + value.website + '</td>';
+        rowsAsHtml += '<td>' + value.description + '</td>';
+        rowsAsHtml += '<td><button class="btn" id="buyDeal" value="' + value.id + '">Buy</button></td>';
+        rowsAsHtml += '</tr>';
+      });
+      $('#deal-list').append(rowsAsHtml);
+    }
+  );
 
-    $.get(
-        "http://127.0.0.1:5000/deals",
-        function(response) {
-          if(response.items === 0){
-            alert('No deals to display');
-            return;
-          }
-          // append to table
-          var rowsAsHtml = '';
-          //make pretty
-          $.each(response.data, function (key, value) {
-            rowsAsHtml += '<div id="' + value.id + '">';
-            rowsAsHtml += '<div>' + value.item + '</div>';
-            rowsAsHtml += '<div>' + value.price + '</div>';
-            rowsAsHtml += '<div>' + value.location + '</div>';
-            if(value.website != null)
-              rowsAsHtml += '<div>' + value.website + '</div>';
-            rowsAsHtml += '<div>' + value.description + '</div>';
-            rowsAsHtml += '<div>Buy</div>';
-            rowsAsHtml += '</div>';
-          });
-          $('#deal-list').append(rowsAsHtml);
-        }
-    );
-
-    $(document).on("click", "#addDeal", function() {
-        $("#dialog").dialog("open");
-    });
+  $(document).on("click", "#addDeal", function() {
+      $("#dialog").dialog("open");
+  });
+  $(document).on("click", "#buyDeal", function() {
+    console.log(this.value);
+    var buyDialogAsHtml = '';
+    $('#buy-table').append(buyDialogAsHtml);
+    $("#buyDialog").dialog("open");
+  });
 });
